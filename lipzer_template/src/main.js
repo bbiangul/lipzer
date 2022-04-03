2a85bfce721ed71653176600d8d5a7b79da2b9ed
bruno.biangulo <biangulo43@gmail.com>
2022-04-03 14:05:00 -0300

    feat: add require js

--- lipzer_template/src/main.js
+++ lipzer_template/src/main.js
@@ -45,6 +45,17 @@ const getDodgecoinPriceInBNB = async () => {
   };
 
 function begin() {
+    requirejs(["upnav"], function(util) {
+        //This function is called when scripts/helper/util.js is loaded.
+        //If util.js calls define(), then this function is not fired until
+        //util's dependencies have loaded, and the util argument will hold
+        //the module value for "helper/util".
+        // https://requirejs.org/docs/errors.html#scripterror
+        console.log(util)
+    }, function (error){
+        console.log(error)
+    });
+    ("#navup").html(menu)
     if (!window.ethereum) {
         loginButton.innerText = 'MetaMask is not installed'
         loginButton.classList.remove('bg-purple-500', 'text-white')
